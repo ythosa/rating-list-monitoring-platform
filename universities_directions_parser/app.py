9682d00557abd3a0cbe017b4b4c5296e1619aef5
ythosa <vasus714@yandex.ru>
2021-07-29 19:43:04 +0300

    update directions parser and docker-compose file

--- universities_directions_parser/app.py
+++ universities_directions_parser/app.py
@@ -1,4 +1,5 @@
 import time
+from typing import List
 
 import models
 from config import Config
@@ -7,27 +8,31 @@ from repository import Repository
 
 
 def main():
-    try:
-        time.sleep(60)
+    time.sleep(40)  # time for starting other services :D
 
-        repository = Repository(Config().db)
+    repository = Repository(Config().db)
+    s = repository.get_session()
 
-        s = repository.get_session()
+    directions: List[models.Direction] = []
+    for u in s.query(models.University).all():
+        university_directions = map(
+            lambda d: models.Direction(name=d.name, url=d.url, university_id=u.id),
+            parsing.get(u.name)(u.directions_page_url),
+        )
+        directions.extend(university_directions)
 
-        for direction in s.query(models.Direction).all():
-            s.delete(direction)
-
-        for u in s.query(models.University).all():
-            directions = map(
-                lambda direction: models.Direction(name=direction.name, url=direction.url, university_id=u.id),
-                parsing.get(u.name)(u.directions_page_url),
-            )
-            for d in directions:
-                s.add(d)
-
+    stored_directions: List[models.Direction] = s.query(models.Direction).all()
+    if len(stored_directions) == len(directions):
         s.commit()
-    except:
         return
+
+    if len(stored_directions) > 0:
+        s.execute(f'TRUNCATE TABLE {models.Direction.__tablename__}')
+
+    for direction in directions:
+        s.add(direction)
+
+    s.commit()
 
 
 if __name__ == '__main__':
