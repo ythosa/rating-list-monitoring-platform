63f2a21206742dbadcd6b2ebb04b946e8a87cd6c
ythosa <vasus714@yandex.ru>
2021-08-03 05:08:51 +0300

    update migrationgs

--- api/schema/000001_init.up.sql
+++ api/schema/000001_init.up.sql
@@ -13,6 +13,7 @@ CREATE TABLE universities
 (
     id                  serial       not null unique,
     name                varchar(255) not null,
+    full_name           varchar(255) not null,
     directions_page_url varchar(255) not null
 );
 
@@ -38,8 +39,10 @@ CREATE TABLE users_directions
     direction_id int references directions (id) not null
 );
 
-INSERT INTO universities (name, directions_page_url)
+INSERT INTO universities (name, full_name, directions_page_url)
 VALUES ('СПБГУ',
+        'Санкт-Петербургский государственный университет',
         'https://cabinet.spbu.ru/Lists/1k_EntryLists/index_comp_groups.html'),
        ('ЛЭТИ',
+        'Санкт-Петербургский государственный электротехнический университет «ЛЭТИ»',
         'https://etu.ru/ru/abiturientam/priyom-na-1-y-kurs/podavshie-zayavlenie/');
