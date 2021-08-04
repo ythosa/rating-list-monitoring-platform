262239c482f8d7d000e63985a5af4b54ae2f7fda
ythosa <vasus714@yandex.ru>
2021-08-04 12:24:36 +0300

    base of frontend

---
+++ frontend/src/app/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './app.css';
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <Router>
+            </Router>
+        </React.Fragment>
+    );
+};
+
+export default App;
